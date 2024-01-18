362eda00b9fdc7cc34958ad68bc5dacbc71798a1
thomais oliveira da silva <thomaisoliveiradasilva@hotmail.com>
2024-01-17 22:57:46 -0300

    Desafio One

---
+++ app.js
@@ -0,0 +1,17 @@
+alert('Seja bem vindo ao nosso site')
+let nome = 'Lua'
+let idade = 25
+let numeroDeVendas = 50
+let saldoDisponivel = 1000
+alert('Erro! preencha todos os campos')
+let mensagemDeErro ='Erro! Preencha todos os campos.'
+alert(mensagemDeErro)
+nome1 = prompt('Digite o nome do Usuário!') 
+idade1 = prompt('Digite sua idade!')
+
+if(idade1 >= 18 ){
+    confirm(`Boas noticias ${nome1} você Pode tirar a habilitação!`)
+}else{
+    confirm(`Pesiama noticia ${nome1} você Não pode tirar a habilitação!`)
+}
+
